ebe8363caf0ce7c8b50cdc7ce3268639d0f34064
SurbhiSarvaiya <surbhigohil1991@gmail.com>
2026-01-16 12:59:24 +0530

    Update ResultPage.jsx

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-
+import { API } from '../config';
 const ResultPage = () => {
     const { id } = useParams();
     const [result, setResult] = useState(null);
