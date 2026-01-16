39d62365bd5bfea8d7ea344f3d7cba857377145c
SurbhiSarvaiya <surbhigohil1991@gmail.com>
2026-01-16 12:57:37 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { toast } from 'react-toastify';
-
+import { API } from '../config';
 const Login = () => {
     const [mobile, setMobile] = useState('');
     const [password, setPassword] = useState('');
