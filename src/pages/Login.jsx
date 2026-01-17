c08c8e327a0078acc34923609d734da4ea9481ae
SurbhiSarvaiya <surbhigohil1991@gmail.com>
2026-01-17 11:01:09 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,10 @@ import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { toast } from 'react-toastify';
 import { API } from '../config';
-axios.post("https://backend-56qq.onrender.com/login")
+import API from "../api";   
 
-API.post("/login");
+axios.post("https://backend-56qq.onrender.com/login")
+API.post("/login", data);
 const Login = () => {
     const [mobile, setMobile] = useState('');
     const [password, setPassword] = useState('');
