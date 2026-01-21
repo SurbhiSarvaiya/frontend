0bc6adfb2440b606cf9b6d951ee82678b41a17d0
SurbhiSarvaiya <surbhigohil1991@gmail.com>
2026-01-21 16:43:55 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,2 @@
 export const API = import.meta.env.VITE_API_URL || '';
 
-import axios from "axios";
-
-export const API = axios.create({
-  baseURL: "https://backend-56qq.onrender.com",
-});
-
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
